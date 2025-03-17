0e2b5bd2bfc6d906ae239aa66865203a80284021
Beatriz Batista <byah24@outlook.com>
2025-03-17 10:58:25 -0300

    Programa finalizado para envio na lista

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Santos");
+Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("\nSeleção");
+
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+Console.Write(" Brasileira");
+Console.ResetColor();
+Console.WriteLine("\n");
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write(" Live");
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.Write(" ver");
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+Console.Write(" pool");
+
+Console.ResetColor();
+
+
